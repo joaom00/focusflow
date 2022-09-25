55263b491157a0e56eff78e72b2f59fa94f8c99f
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-09-25 12:01:06 -0300

    add pnpm workspace

--- src/utils/seconds.ts
+++
@@ -1,9 +0,0 @@
-function padStartWithZeros(value: number, totalStringSize: number) {
-  return value.toString().padStart(totalStringSize, '0')
-}
-
-export function formatSecondsIntoMinutesAndSeconds(value: number) {
-  const seconds = padStartWithZeros(value % 60, 2)
-  const minutes = padStartWithZeros(Math.floor(value / 60), 2)
-  return [minutes, seconds].join(':')
-}
