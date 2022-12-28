06ba6ff51ff26ce250b6e3129e4ba9dc250fc427
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-12-27 23:11:26 -0300

    chore: remove server folder and rename client to web

--- server/prisma/migrations/20221005010947_add_position_column/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `position` to the `Todo` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Todo" ADD COLUMN     "position" REAL NOT NULL;
--- server/prisma/migrations/20221005133035_update_position_column_type/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `position` on the `Todo` table. The data in that column could be lost. The data in that column will be cast from `Real` to `Integer`.
-
-*/
--- AlterTable
-ALTER TABLE "Todo" ALTER COLUMN "position" SET DATA TYPE INTEGER;
